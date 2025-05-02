cc65bbffc19f1812736c056b8c6cbe3be2f7d969
SuddiaSasha <st8485833@stud.duikt.edu.ua>
2025-05-02 17:01:43 +0300

    left panel & styles
    made a left pannel with currency name and current price, also green styles and edited html file (deleted <span> from div class panel left-panel<h2>

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 fetch(
     'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 )
-.then((response) => response.json())
-.then(res => render(res))
-.catch((err) => console.error(err));
+    .then((response) => response.json())
+    .then(res => render(res))
+    .catch((err) => console.error(err));
 
-function render (arr) {
-    const list = arr.map(rate => `<li>${rate.txt} - ${rate.rate}</li>`).join("");
-    console.log(list)
-    const body = document.body;
-body.innerHTML = `<ul>${list}</ul>`
+function render(arr) {
+    const list = arr.map(rate => `<li class="currency-item">${rate.txt} - ${rate.rate.toFixed(2)} грн</li>`).join("");
+    const currencyList = document.getElementById("currency-list");
+    currencyList.innerHTML = "";
+    currencyList.innerHTML = list;
+
+    const currentDate = document.getElementById("current-date");
+    const today = arr[0].exchangedate;
+    currentDate.textContent = `Курс валют на ${today}`;
 }
